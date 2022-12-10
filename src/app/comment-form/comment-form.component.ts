2137bbcf58e6d2918b124ac42bbafa3fa14ff012
alexandre-leroux <71876330+alexandre-leroux@users.noreply.github.com>
2022-12-10 19:53:03 +0100

    add email and form validation form add comment

--- src/app/comment-form/comment-form.component.ts
+++ src/app/comment-form/comment-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Output, EventEmitter } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
@@ -28,6 +28,10 @@ export class CommentFormComponent {
     name: '',
     body: ''
   });
+  emailValidate: boolean = false
+  nameValidate: boolean = false
+  commentValidate: boolean = false
+  formValidate: boolean = false
 
   constructor(
     private formBuilder: FormBuilder,
@@ -48,6 +52,42 @@ export class CommentFormComponent {
         this.addCommentForm.reset();
         this.loader = false;
       })
+  }
+
+  enableButtonSubmit(emailValidate: boolean, nameValidate: boolean, commentValidate: boolean) {
+    if (emailValidate && nameValidate && commentValidate) {
+      this.formValidate = true
+    }
+  }
+  checkEmail(event: string) {
+    const regex = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
+    if (regex.test(event)) {
+      this.emailValidate = true
+      this.enableButtonSubmit(this.emailValidate, this.nameValidate, this.commentValidate)
+    }
+    else {
+      this.emailValidate = false
+      this.formValidate = false
+    }
+  }
+
+  checkNameNotEmpty(event: string) {
+    if (event != '') {
+      this.nameValidate = true
+      this.enableButtonSubmit(this.emailValidate, this.nameValidate, this.commentValidate)
+    } else {
+      this.nameValidate = false
+      this.formValidate = false
+    }
+  }
+  checkCommentNotEmpty(event: string) {
+    if (event != '') {
+      this.commentValidate = true
+      this.enableButtonSubmit(this.emailValidate, this.nameValidate, this.commentValidate)
+    } else {
+      this.commentValidate = false
+      this.formValidate = false
+    }
   }
 
 }
